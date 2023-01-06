b8b912f1d402adb46a5513421fd17375f15e592a
Léana 江 <leana.jiang@icloud.com>
2023-01-06 13:05:06 +0100

    feat: normalize_tracknumber

--- manage_music_metadata.py
+++ manage_music_metadata.py
@@ -3,11 +3,20 @@ from sys import argv
 from os.path import join, isdir, isfile
 from os import path, listdir
 import argparse
+import music_tag
 
 
 FILE_EXT = ["m4a", "flac"]
+QUIET = False
 PATH = ""
 RUN = False
+FILES = []
+TRACKS = []
+
+
+def load_files():
+    global FILES
+    FILES = find_files(PATH)
 
 
 def find_files(root: str) -> list[str]:
@@ -22,17 +31,37 @@ def find_files(root: str) -> list[str]:
     return files
 
 
+def load_tags():
+    global TRACKS
+    TRACKS = [
+        (music_tag.load_file(file), file)
+        for file in FILES
+    ]
+
+
 def parse():
     global PATH
     global RUN
     parser = argparse.ArgumentParser()
     parser.add_argument("path")
     parser.add_argument("--run", action="store_true")
+    parser.add_argument("--quiet", action="store_true")
     args = parser.parse_args()
     PATH = args.path
     RUN = args.run
 
+def normalize_tracknumber():
+    for track, path in TRACKS:
+        if not QUIET:
+            print(f'{track.raw["tracknumber"]} -> {track["tracknumber"]}')
+        track["tracknumber"] = track["tracknumber"]
+        if RUN:
+            track.save()
+
+
+
 if __name__ == "__main__":
     parse()
-    
-    
+    load_files()
+    load_tags()
+    normalize_tracknumber()
