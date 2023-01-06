cdb8a536ddebff66b7aee6901db697304ac71df7
Léana 江 <leana.jiang@icloud.com>
2023-01-06 11:12:08 +0100

    add(Parser): option parser

--- manage_music_metadata.py
+++ manage_music_metadata.py
@@ -1,16 +1,20 @@
 
 from sys import argv
-from os.path import join, isdir, isfile, basename
+from os.path import join, isdir, isfile
 from os import path, listdir
+import argparse
+
 
 FILE_EXT = ["m4a", "flac"]
+PATH = ""
+RUN = False
 
 
 def find_files(root: str) -> list[str]:
     files = []
     for entry_name in listdir(root):
         entry_path = join(root, entry_name)
-        extension = path.splitext(entry_name)[1].strip('.')
+        extension = entry_name.split('.')[-1]
         if isfile(entry_path) and extension.lower() in FILE_EXT:
             files.append(entry_path)
         elif isdir(entry_path):
@@ -18,4 +22,17 @@ def find_files(root: str) -> list[str]:
     return files
 
 
-print(find_files("/Users/leana/test/"))
+def parse():
+    global PATH
+    global RUN
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path")
+    parser.add_argument("--run", action="store_true")
+    args = parser.parse_args()
+    PATH = args.path
+    RUN = args.run
+
+if __name__ == "__main__":
+    parse()
+    
+    
